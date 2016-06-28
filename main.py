def97b122785b5c22d9743dab58228e7dab483a0
juanjo <juanjo.rubio@est.fib.upc.edu>
2016-06-28 22:06:42 +0200

    first attempt with real node

--- main.py
+++ main.py
@@ -14,32 +14,47 @@ filename = "query_output1000.json"
 print "Creating graph . . ."
 graph = create_graph(users,1)
 
+id_mamon = []
+for elem in users.iteritems():
+    if 'boza' in strip_accents(elem[1]['real_name'].encode('utf-8').lower()):
+        id_mamon.append(elem[0])
+
+print id_mamon
+print users[id_mamon[0]]['real_name']
+# https://twitter.com/josecarlosboza
+print users[id_mamon[0]]['neighbours']
+
+beta = 0.00001
+infected = [int(id_mamon[0])]
+Nrep = 500
+Nsteps = 3
+simulation(graph, users, beta, infected, Nrep, Nsteps)
 # Obtain degree distribution
-deg_dist = degree_dist(graph)
-
-bins = [tup[0] for tup in list(deg_dist.bins())]
-vals = [tup[2] for tup in list(deg_dist.bins())]
-
-# Esperamos a tener mas nodos...de momento es muy pobre con todo
-plt.hist(vals[0:50], bins[0:50])
-plt.show()
-
-# Verified users from dataset
-verified = get_verified(users)
-# Obtain communities
-
-print "Finding communities . . . "
-partition = find_comm(graph,1)
-
-for i in range(3):
-    get_cluster_nodes(graph, partition[i], filename,i)
-
-
-print "Looking for most popular users . . ."
-#Get user who are hubs of the network
-ind = get_hubs(graph, 10)
-
-get_twitter_info(graph, ind, filename)
-
-print "Writing graph in pajek format"
-graph.write_pajek("1000.net")
+# deg_dist = degree_dist(graph)
+#
+# bins = [tup[0] for tup in list(deg_dist.bins())]
+# vals = [tup[2] for tup in list(deg_dist.bins())]
+#
+# # Esperamos a tener mas nodos...de momento es muy pobre con todo
+# plt.hist(vals[0:50], bins[0:50])
+# plt.show()
+#
+# # Verified users from dataset
+# verified = get_verified(users)
+# # Obtain communities
+#
+# print "Finding communities . . . "
+# partition = find_comm(graph,1)
+#
+# for i in range(3):
+#     get_cluster_nodes(graph, partition[i], filename,i)
+#
+#
+# print "Looking for most popular users . . ."
+# #Get user who are hubs of the network
+# ind = get_hubs(graph, 10)
+#
+# get_twitter_info(graph, ind, filename)
+#
+# print "Writing graph in pajek format"
+# graph.write_pajek("1000.net")
